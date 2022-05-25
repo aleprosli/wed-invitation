11b2dfe15dfbcbef41fe9e36b1962c704bd72792
aliff <aliff.rosli96@gmail.com>
2022-05-26 02:31:22 +0800

    route updated

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [App\Http\Controllers\MainController::class, 'index'])->name('main.index');
+Route::get('/kehadiran', [App\Http\Controllers\AttendanceController::class, 'index'])->name('attendance.index');
 
 Auth::routes();
 
