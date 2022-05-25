f506d75c31164120fd553e2c6ef6b1d5bed17a15
aliff <aliff.rosli96@gmail.com>
2022-05-26 02:13:36 +0800

    add content for main

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -5,6 +5,9 @@
     body {
         font-family: 'Poppins';font-size: 22px;
     }
+
+    h1 { font-family: 'Berkshire Swash'}
+    
 </style>
 
 <div class="container bg-white">
@@ -16,6 +19,42 @@
         </div>
         <div class="row text-muted text-center">
             <h6> Assalamualaikum W.B.T </h6>
+        </div>
+        <div class="row text-center">
+            <h1> Aliff </h1>
+        </div>
+        <div class="row text-center">
+            <h1> & </h1>
+        </div>
+        <div class="row text-center">
+            <h1> Izzati </h1>
+        </div>
+        <div class="row text-muted text-center mt-2">
+            <h6> Dengan segala hormatnya menjemput </h6>
+        </div>
+        <div class="row text-center">
+            <h5> Dato’ / Datin / Tuan / Puan / Encik / Cik </h5>
+        </div>
+        <div class="row text-muted text-center mt-2">
+            <h6> suami/isteri sekeluarga ke majlis pertunangan putera  kami bersama pilihan hatinya </h6>
+        </div>
+        <div class="row text-center mt-2">
+            <h6> Bertempat di</h6>
+        </div>
+        <div class="row text-muted text-center mt-2">
+            <div class="grid grid-cols-2">
+                <a href="#" class="btn btn-sm btn-light">Google Maps</a>
+                <a href="#" class="btn btn-sm btn-light">Waze</a>
+            </div>
+        </div>
+        <div class="row text-muted text-center mt-4">
+            <h6> Pada Tarikh/Hari</h6>
+        </div>
+        <div class="row text-center mt-2">
+            <h1> Tarikh September, Hari</h1>
+        </div>
+        <div class="row px-4 mt-3">
+           <a type="button" href="#" class="btn btn-block btn-dark poppins">Tekan untuk kehadiran <i class='bx bxs-direction-left'></i></a>
         </div>
     </div>
 </div>
