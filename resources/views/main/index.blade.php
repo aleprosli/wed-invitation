698012372f741ed6bac9a00f1520277643027c31
aliff <aliff.rosli96@gmail.com>
2022-05-26 01:36:43 +0800

    add simple text and style

--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -1,10 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
+<style>
+    body {
+        font-family: 'Poppins';font-size: 22px;
+    }
+</style>
+
 <div class="container bg-white">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12 text-center px-2">
+            <i class='bx bx-male-female'></i>
+            <h6>Undangan Majlis Pertunangan</h6>
             
+        </div>
+        <div class="row text-muted text-center">
+            <h6> Assalamualaikum W.B.T </h6>
         </div>
     </div>
 </div>
