70b526fe5226a138367748f16a3c10636db426e5
aliff <aliff.rosli96@gmail.com>
2022-05-26 21:28:45 +0800

    create gift controller with store function with toyyibpay

--- app/Http/Controllers/GiftController.php
+++ app/Http/Controllers/GiftController.php
@@ -10,4 +10,37 @@ class GiftController extends Controller
     {
         return view('gift.index');
     }
+
+    public function store(Request $request)
+    {
+        $toyyibpay_secret_key = config('services.toyyibpay.secret');
+
+        $url = config('services.toyyibpay.url').'createBill';
+
+        $body =[
+            'userSecretKey' => $toyyibpay_secret_key,
+            'categoryCode' => config('services.toyyibpay.categoryCode'),
+            'billName' => $request->name,
+            'billDescription' => 'Hadiah bernilai : RM '.$request->price,
+            'billAmount' => $request->price,
+            'billReturnUrl'=>route('returnURL'),
+            'billCallbackUrl'=>route('callbackURL'),
+            'billExternalReferenceNo' => $request->id,
+            'billPayorInfo'=>1,
+            'billPhone'=> $request->phone_number,
+            'billTo'=> '',
+            'billEmail'=> $request->email,
+            'billPriceSetting'=>0,
+            'billContentEmail'=>'Terima Kasih untuk permberiah hadiah bernilai : RM '.$request->price,
+            'billChargeToCustomer'=>''
+        ];
+
+        $response = Http::asForm()->post($url, $body);
+       
+        $bill_code = $response->json()[0]['BillCode'];
+
+        $purchase->update(['toyyibpay_bill_code' => $bill_code]);
+
+        return to_route('main:index');
+    }
 }
