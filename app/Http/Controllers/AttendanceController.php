964349ec44a979c9c8251906c056a78f268010af
aliff <aliff.rosli96@gmail.com>
2022-05-26 15:27:33 +0800

    add AttendanceRequest, change create to updateorcreate

--- app/Http/Controllers/AttendanceController.php
+++ app/Http/Controllers/AttendanceController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Attendance;
 use Illuminate\Http\Request;
 use App\Jobs\SendAttendanceJob;
+use App\Http\Requests\AttendanceRequest;
 
 class AttendanceController extends Controller
 {
@@ -13,13 +14,16 @@ class AttendanceController extends Controller
         return view('attendance.index');
     }
 
-    public function store(Request $request)
+    public function store(AttendanceRequest $request)
     {
-        $attendance =  Attendance::create([
+        $attendance =  Attendance::updateOrCreate([
+            'email' => $request->email
+        ],
+        [
             'name' => $request->name,
             'email' => $request->email
         ]);
-        
+
         SendAttendanceJob::dispatch($attendance);
         
         return to_route('main.index');
