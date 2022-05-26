839a9edc7b8ecb0f61e8b5265e841771c5711c4d
aliff <aliff.rosli96@gmail.com>
2022-05-26 15:51:27 +0800

    create new blade for gift

---
+++ resources/views/gift/index.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+<style>
+    body {
+        font-family: 'Poppins';font-size: 22px;
+    }
+
+    h1 { font-family: 'Berkshire Swash'}
+    
+</style>
+
+<div class="container bg-white">
+    <div class="row justify-content-center">
+        <div class="col-md-12 text-center px-2">
+            <i class='bx bxs-gift'></i>
+            <h6>Hadiah</h6>
+            
+        </div>
+    </div>
+</div>
+@endsection
