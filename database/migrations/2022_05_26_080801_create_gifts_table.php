69728f41110924f2600ee26e20aefaf06b8343ba
aliff <aliff.rosli96@gmail.com>
2022-05-26 21:28:59 +0800

    create gift tables

---
+++ database/migrations/2022_05_26_080801_create_gifts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gifts', function (Blueprint $table) {
+            $table->id();
+            $table->uuid('uuid')->nullable();
+            $table->string('name');
+            $table->float('price', 8, 2);
+            $table->string('phone_number');
+            $table->string('email')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gifts');
+    }
+};
